825ce0ea4632fd127aa69e6f3da9b5d826084ca8
HUGE | Rob La Placa <rlaplaca@hugeinc.com>
2014-06-02 19:03:45 -0400

    setting up resting state, and objects

--- assets/js/microphone.js
+++ assets/js/microphone.js
@@ -1,69 +1,142 @@
-//http://www.jjask.com/379926/get-audio-levels-from-html5-audio-microphone-stream
-//http://stackoverflow.com/questions/15900103/html5-audio-buffer-getting-stuck
-var context = new webkitAudioContext();
 
-navigator.webkitGetUserMedia({audio: true}, function(stream) {
-	liveSource = context.createMediaStreamSource(stream);
-	liveSource.connect(context.destination);
+var HueLight = function(params) {
+	var opts = $.extend({
+		lightId: null
+	}, params);
 
-	var levelChecker = context.createJavaScriptNode(4096, 1 ,1);
-	liveSource.connect(levelChecker);
+	var MAX_VALUE = 65535,
+		previousValue = 0.1,
+		RESTING_STATE = 0.2,
+		changeTimeout = null;
 
-	levelChecker.connect(context.destination);
-
-	levelChecker.onaudioprocess = window.audioProcess = function(e) {
-		var maxVal = getAmplitude(e);
-		
-		if (maxVal <= 0.1) {
-			changeLight('off', maxVal);
-		} else {
-			changeLight('on', maxVal);
-		}
-	};
-});
-
-function getAmplitude(e) {
-	var buffer = e.inputBuffer.getChannelData(0),
-		maxVal = 0;
-	
-	for (var i = 0; i < buffer.length; i++) {
-		if (maxVal < buffer[i]) {
-			maxVal = buffer[i];
-		}
-	}
-
-	return maxVal;
-}
-
-function changeLight(state, maxVal) {
 	var lightData = {
 		"on":true,
 		"sat":255,
 		"bri":255,
-		"hue":10000
+		"hue":0
 	};
 
-	if (state === 'on') {
-		lightData.on = false;
-		lightData.hue = parseInt(maxVal * 400000, 10);
-		lightData.bri = parseInt(maxVal * 5000, 10);
-		lightData.sat = parseInt(maxVal * 100000, 10);
-	} else {
-		lightData.on = true;
-		lightData.hue = parseInt(maxVal * 10000, 10);
-		lightData.bri = parseInt(maxVal * 10000, 10);
-		lightData.sat = parseInt(maxVal * 10000, 10);
+	function init() {
+		gotoRestingState();
 	}
 
-	$.ajax({
-		type: "PUT",
-		url: "http://10.10.2.163/api/newdeveloper/lights/1/state",
-		data: JSON.stringify(lightData),
-		success: function(data){
-			//console.log('DATA?', data);
-			console.log(lightData.hue);
-		},
-		dataType: "json",
-		contentType : "application/json"
+	function changeLight(state, amplitude, forceUpdate) {
+		var delta = Math.abs(previousValue - amplitude),
+			THRESHOLD = 0.3;
+
+		// console.log("delta: ", delta, " threshold: ", THRESHOLD);
+		lightData.on = state;
+
+		if( delta > THRESHOLD || forceUpdate === true) {
+			if( forceUpdate ) {
+				lightData.hue = parseInt(MAX_VALUE * RESTING_STATE, 10);
+				lightData.bri = parseInt(100, 10);
+				lightData.sat = parseInt(0, 10);
+			} else {
+				lightData.hue = parseInt(MAX_VALUE * amplitude, 10);
+				lightData.bri = parseInt(100, 10);
+				lightData.sat = parseInt(255, 10);
+			}	
+
+			$.ajax({
+				type: "PUT",
+				url: "http://10.10.2.163/api/newdeveloper/lights/" + opts.lightId + "/state",
+				data: JSON.stringify(lightData),
+				success: function(data){
+					//console.log('DATA?', data);
+					// console.log(lightData.hue);
+					previousValue = amplitude;
+					clearTimeout(changeTimeout);
+					setChangeTimeout();
+				},
+				dataType: "json",
+				contentType : "application/json"
+			});
+		}
+	}
+
+	function setChangeTimeout() {
+		changeTimeout = setTimeout(function() {
+			console.clear();
+			console.log('GO BACK TO RESTING STATE');
+			gotoRestingState();
+		}, 300);
+	}
+
+	function gotoRestingState() {
+		changeLight("on", RESTING_STATE, true);
+	}
+
+	init();
+
+	return {
+		changeLight: changeLight
+	};
+};
+
+// http://www.jjask.com/379926/get-audio-levels-from-html5-audio-microphone-stream
+// http://stackoverflow.com/questions/15900103/html5-audio-buffer-getting-stuck
+
+var Microphone = function(params) {
+	var opts = $.extend({
+		onProcessAudo: $.noop
+	}, params);
+
+	function init() {
+		var context = new webkitAudioContext();
+
+		// console.log( typeof context );
+		// console.log( typeof context.createScriptProcessor );
+
+		navigator.webkitGetUserMedia({audio: true}, function(stream) {
+			liveSource = context.createMediaStreamSource(stream);
+			liveSource.connect(context.destination);
+
+			var levelChecker = context.createScriptProcessor(4096, 1 ,1);
+			liveSource.connect(levelChecker);
+
+			levelChecker.connect(context.destination);
+
+			levelChecker.onaudioprocess = window.audioProcess = opts.onProcessAudio;
+		}, function() {
+			// error state
+		});
+	}
+
+	function getAmplitude(e) {
+		var buffer = e.inputBuffer.getChannelData(0),
+			maxVal = 0;
+		
+		for (var i = 0; i < buffer.length; i++) {
+			if (maxVal < buffer[i]) {
+				maxVal = buffer[i];
+			}
+		}
+
+		return maxVal;
+	}
+
+	init();
+
+	return {
+		getAmplitude: getAmplitude
+	};
+};
+
+$(function() {
+	var light1 = new HueLight({
+		lightId: 1
 	});
-}
+
+	var mic = new Microphone({
+		onProcessAudio: function(e) {
+			var amplitude = mic.getAmplitude(e),
+				lightOn = true;
+
+			console.log(amplitude);
+			// var lightOn = (amplitude >= 0.2) ? true : false;
+
+			light1.changeLight(lightOn, amplitude);	
+		}
+	});
+});
