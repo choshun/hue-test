c8f2ae27eb811a0048937a046090cb84be518688
HUGE | Rob La Placa <rlaplaca@hugeinc.com>
2014-06-03 12:16:03 -0400

    adding comments to HueLight.js

--- assets/js/components/HueLight.js
+++ assets/js/components/HueLight.js
@@ -1,36 +1,57 @@
+/**
+ * Single instance of a Hue Light, can be used to control 
+ * it's various properties.
+ *
+ * @class HueLight
+ * @typedef  {config}  params - configuration parameters.
+ * @property {Integer} lightId - the unique id for a light
+ */
+
 var HueLight = function(params) {
 	var opts = $.extend({
 		lightId: null
 	}, params);
 
 	var MAX_VALUE = 65535,
-		previousValue = 0.1,
 		RESTING_STATE = 0.2,
-		changeTimeout = null;
-
-	var lightData = {
-		"on":true,
-		"sat":255,
-		"bri":255,
-		"hue":0
-	};
+		previousValue = 0.1,
+		changeTimeout = null,
+		lightData = {
+			"on":true,
+			"sat":255,
+			"bri":255,
+			"hue":0
+		};
+	
+	/**
+     * @constructor
+     */
 
 	function init() {
 		gotoRestingState();
 	}
 
+	/**
+	 * Sets the hue light to a new state 
+	 *
+	 * @param {Boolean} state
+	 * @param {Number} amplitude - value between 0 and 1 indicating volume
+	 * @param {Boolean} forceUpdate
+     * @public
+     */
+
 	function changeLight(state, amplitude, forceUpdate) {
 		var delta = Math.abs(previousValue - amplitude),
 			THRESHOLD = 0.5;
 
-		// console.log("delta: ", delta, " threshold: ", THRESHOLD);
 		lightData.on = state;
 		lightData.transitiontime = 2;
 
+		/* 
+			TODO: make changeLight more generic by removing amplitude since that
+			is implementation specific
+		*/
 		if( delta > THRESHOLD || forceUpdate === true) {
-			console.clear();
-			console.log('CHANGE STATE');
-
 			if( forceUpdate ) {
 				lightData.hue = parseInt(MAX_VALUE * RESTING_STATE, 10);
 				lightData.bri = parseInt(100, 10);
@@ -39,15 +60,18 @@ var HueLight = function(params) {
 				lightData.hue = parseInt(MAX_VALUE * amplitude, 10);
 				lightData.bri = parseInt(100, 10);
 				lightData.sat = parseInt(255, 10);
-			}	
+			}
 
+			/*
+			 * TODO: remove hardcoded ip address
+			 * TODO: add debug mode where a dom element can be updated to simulate the light
+			 * TODO: break success out into a callback instances can execute
+			 */
 			$.ajax({
 				type: "PUT",
 				url: "http://10.10.2.163/api/newdeveloper/lights/" + opts.lightId + "/state",
 				data: JSON.stringify(lightData),
 				success: function(data){
-					//console.log('DATA?', data);
-					// console.log(lightData.hue);
 					previousValue = amplitude;
 					clearTimeout(changeTimeout);
 					setChangeTimeout();
@@ -58,11 +82,24 @@ var HueLight = function(params) {
 		}
 	}
 
+	/**
+	 * Sets timeout used to throttle hue light by preventing it
+	 * from returning to default state until a duration is elapsed
+	 *
+     * @private
+     */
+
 	function setChangeTimeout() {
 		changeTimeout = setTimeout(function() {
 			gotoRestingState();
 		}, 5000);
 	}
+
+	/**
+	 * Returns light back to pre-defined default state
+	 *
+     * @private
+     */
 
 	function gotoRestingState() {
 		changeLight("on", RESTING_STATE, true);
